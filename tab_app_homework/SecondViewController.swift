86d38718bfc08f6b434e35aa8cdb240799c67e45
aquawill <aquawill@msn.com>
2017-02-13 23:08:08 +0800

    Initial Commit

---
+++ tab_app_homework/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tab_app_homework
+//
+//  Created by Wu, Guan-Ling on 2017/2/13.
+//  Copyright © 2017年 Wu, Guan-Ling. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
